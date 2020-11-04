780e9a946aaf11cf05adca804fe45b515cb3ac0e
xavier <xavierdavidgutierrez@gmail.com>
2020-11-03 22:09:58 -0300

    feature: add database config

--- src/main/java/com/xvierd/challenge/user/infrastructure/configuration/DatabaseConfig.java
+++ src/main/java/com/xvierd/challenge/user/infrastructure/configuration/DatabaseConfig.java
@@ -6,8 +6,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("cloud")
 public class DatabaseConfig {
 
   @Value("${spring.datasource.url}")
